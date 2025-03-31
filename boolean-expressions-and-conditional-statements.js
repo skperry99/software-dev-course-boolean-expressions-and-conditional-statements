cfea56a8a45745342266700cdcfe9aaa594b721c
Sarah <skat1403@gmail.com>
2025-03-31 14:12:58 -0500

    added additional items and choices

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -28,12 +28,30 @@ const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+const hasSword = true;
+const hasMedicine = false;
+const hasCompass = false;
+
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
-const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+const choice = readline.question("Do you go to the 'mountains' or the 'village' or go back 'home'?\n");
 
-if (choice === "mountains" && hasTorch) {
+if (choice === "home") {
+  console.log("You go back in the house and curl up on the couch with a good book.");
+}
+  else if (choice === "mountains" && hasTorch) {
   console.log("You safely navigate through the dark mountains.");
+  const secondChoice = readline.question("You see a 'lake' and a 'cabin', which do you choose to investigate?\n");
+
+  if (secondChoice === "lake") {
+    console.log("You approach the lake and find a small wooden boat.");
+  }
+    else if (secondChoice === "cabin") {
+    console.log("You enter the cabin and the door slams shut behind you.");
+  }
+    else {
+    console.log("You decide to explore later and lay down to take a nap.");
+  }
 } else if (choice === "mountains" && !hasTorch) {
   console.log("It's too dark to proceed. You decide to turn back.");
 } else if (choice === "village" || hasMap) {
